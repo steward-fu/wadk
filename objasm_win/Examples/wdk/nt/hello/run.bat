525ddaf313f78c1df72c89412dd12d9fc00a2a4f
steward-fu <steward.fu@gmail.com>
2026-08-20 15:53:35 +0800

    Refine objasm folder.

--- objasm_win/Examples/wdk/nt/hello/run.bat
+++
@@ -1,3 +0,0 @@
-call start /B c:\dbgview.exe
-c:\sleep.exe 3
-call start c:\kmdmanager.exe
--- objasm_win/Examples/wdk/wdm/hello/run.bat
+++
@@ -1,5 +0,0 @@
-call start /B c:\dbgview.exe
-for /l %%x in (1, 1, 3) do (
-    c:\devcon install e:\main.inf *mydriver
-    c:\devcon remove e:\main.inf *mydriver
-)
